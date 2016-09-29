de19ea572f8a2782a5f1c5bf1b255caf3289cb1e
nrebhun <nick@nrebhun.com>
2016-09-29 16:15:02 -0700

    Initial commmit; added and committed via Fabfile task

---
+++ fabfile.py
@@ -0,0 +1,25 @@
+from fabric.api import task, local
+
+
+@task
+def hello(name="World", descriptor="awesome"):
+    print ("Hello %s!\nYou're %s" % (name, descriptor))
+
+
+def add():
+    local('git add .')
+
+
+def commit():
+    local('git commit')
+
+
+def push(options=""):
+    local('git push %s origin `git branch | grep \* | cut -d ' ' -f2`' % options)
+
+
+@task
+def deploy():
+    add()
+    commit()
+    push()
